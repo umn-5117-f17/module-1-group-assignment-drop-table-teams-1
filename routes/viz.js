481a6a37c9e36a8d66851b747abe56a24a6205b4
BenjiFischman <benjiricefischman@gmail.com>
2017-09-28 07:18:26 -0500

    Template and d3 set up for viz

--- routes/viz.js
+++ routes/viz.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	console.log('Route viz test');
+    res.render('viz', { title: 'Viz' });
+    console.log('Route viz test');
+   // res.send('viz test');
 });
 
 module.exports = router;
